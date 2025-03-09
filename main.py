e9dfef5e993e8bf441ed68623b61c2f357876ca9
Erennyy <114980557+Erennyy@users.noreply.github.com>
2025-03-10 02:51:16 +0300

    Final result

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 def int_to_roman(num):
-    """
-    Convert an integer to a Roman numeral.
 
-    :param num: Integer value between 1 and 3999 inclusive.
-    :return: A string representing the Roman numeral of the integer.
-    """
+    one = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    ten = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    hundred = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    thousand = ["", "M", "MM", "MMM"]
+
+    return thousands[num // 1000] + \
+           hundreds[(num % 1000) // 100] + \
+           tens[(num % 100) // 10] + \
+           ones[num % 10]
